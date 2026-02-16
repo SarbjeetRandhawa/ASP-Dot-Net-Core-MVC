01ded084dfd0456f440602d3099bcf2037d48893
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-16 13:05:18 +0530

    first commit

---
+++ Practice/WebApplication1/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.Use(async (Context , Next) =>
+{
+    await (Context.Response.WriteAsync("this is asp .net core 10\n"));
+    await Next(Context);
+
+});
+app.Run(async (Context) =>
+{
+    await (Context.Response.WriteAsync("this is asp .net core 10"));
+});
+
+app.Run();
