d463678babe4f65076d920712426dde15c0e2335
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-18 15:18:14 +0530

    Form basic

---
+++ BASIC-FORM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BASIC_FORM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BASIC-FORM/Models/FormData.cs
@@ -0,0 +1,14 @@
+﻿namespace BASIC_FORM.Models
+{
+    public class FormData
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Gender Gender { get; set; }
+        public string Married { get; set; }
+    }
+    public enum Gender
+    {
+        Male,Female
+    }
+}
