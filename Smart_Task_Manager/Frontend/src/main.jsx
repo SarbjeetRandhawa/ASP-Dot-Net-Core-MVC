e37876fbb08b20d18600e73277c3c3f89ca9b503
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-04-08 09:44:47 +0530

    created taskdetail ui

--- Smart_Task_Manager/Frontend/src/main.jsx
+++ Smart_Task_Manager/Frontend/src/main.jsx
@@ -2,16 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Mouse from "./Components/CustomMouse/Mouse.jsx";
-
 import { store } from "./Redux/store.js";
 import { Provider } from "react-redux";
+import CustomCursor from "./Components/CustomMouse/Mouse.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+
       <App />
-      <Mouse/>
+      {/* <CustomCursor/> */}
+     
     </Provider>
   </StrictMode>,
 );
